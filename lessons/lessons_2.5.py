6cf43b03e5b548bb082dae66e3e40f37dccfb9f7
Elena Kogai <kogai.lena@gmail.com>
2024-02-12 00:38:52 +0500

    add lessons

---
+++ lessons/lessons_2.5.py
@@ -0,0 +1,8 @@
+import requests
+
+response = requests.get("https://playground.learnqa.ru/api/get_301", allow_redirects=True)
+first_response = response.history[0]
+second_response = response
+
+print(first_response.url)
+print(second_response.url)
