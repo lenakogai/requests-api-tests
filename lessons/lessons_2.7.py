f8d807cd5a9fb0c1391891569a742b6b8380f4e8
Elena Kogai <kogai.lena@gmail.com>
2024-02-14 00:12:58 +0500

    add extra homework

--- lessons/lessons_2.7.py
+++ lessons/lessons_2.7.py
@@ -7,6 +7,7 @@ payload = {
 response1 = requests.post("https://playground.learnqa.ru/api/get_auth_cookie", data=payload)
 
 cookie_value = response1.cookies.get('auth_cookie')
+print(cookie_value)
 
 cookies = {}
 if cookie_value is not None:
