d902e8087bbb02719ec70a45c86b075d005f5641
Elena Kogai <kogai.lena@gmail.com>
2024-02-15 00:13:06 +0500

    fixed lessons_2.7

--- homework/homework_2.4.py
+++ homework/homework_2.4.py
@@ -6,23 +6,20 @@ response_splash_data = requests.get("https://en.wikipedia.org/wiki/List_of_the_m
 soup = BeautifulSoup(response_splash_data.text, 'lxml')
 parser_data = soup.find_all('table', class_="wikitable")[1].find_all('td', align="left")
 splash_data = []
-auth_cookie = ""
-pass_data = ""
-text = ""
 
-for i in range(225):
+for i in range(len(parser_data)):
     parser_text = parser_data[i].getText()
     splash_data.append(parser_text.rstrip('\n'))
 uniq_splash_data = list(dict.fromkeys(splash_data))
 
 # Сохраняем куки и получаем валидный пароль
-
 for i in range(len(uniq_splash_data)):
     pass_data = uniq_splash_data[i]
     response_pass = requests.post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework", data={"login": "super_admin", "password": pass_data})
     auth_cookie = response_pass.cookies.get('auth_cookie')
-    response_cookie = requests.get("https://playground.learnqa.ru/ajax/api/check_auth_cookie", params={"auth_cookie": auth_cookie})
+    response_cookie = requests.get("https://playground.learnqa.ru/ajax/api/check_auth_cookie", cookies={"auth_cookie": auth_cookie})
     text = response_cookie.text
-    if text != "You are NOT authorized":
-        print(pass_data, text)
-
+    if text == "You are authorized":
+        print("Valid password:", pass_data)
+        print(text)
+        break
